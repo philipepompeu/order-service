457f738a60919948a10d47f6a2f0d09dfd10aa48
philipepompeu <philipepompeu2@gmail.com>
2025-03-26 21:49:39 -0300

    criação dos endpoints

--- src/main/java/com/github/philipepompeu/order_service/OrderServiceApplication.java
+++ src/main/java/com/github/philipepompeu/order_service/OrderServiceApplication.java
@@ -3,7 +3,11 @@ package com.github.philipepompeu.order_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info= @Info(title = "Order Service RestFul API", description = "Manage sales orders"))
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/github/philipepompeu/order_service/controllers/ClientController.java
@@ -0,0 +1,46 @@
+package com.github.philipepompeu.order_service.controllers;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@RequestMapping("/clients")
+@Tag(name="Clients",
+     description="List, create, delete and update clients for your orders.")
+public class ClientController {
+
+    @PostMapping()
+    @Operation(summary = "...")
+    public ResponseEntity<?> create(){
+        return ResponseEntity.ok().build();
+    }
+    
+    @DeleteMapping("/{id}")
+    @Operation(summary = "...")
+    public ResponseEntity<?> delete(){
+        return ResponseEntity.ok().build();
+    }
+    
+    @PutMapping
+    @Operation(summary = "...")
+    public ResponseEntity<?> update(){
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    @Operation(summary = "...")
+    public ResponseEntity<List<?>> list(){        
+        return ResponseEntity.ok().build();
+    }
+    
+}
